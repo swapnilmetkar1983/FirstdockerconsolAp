c9eb301fee97b0a783dc3fee8d633e9486533810
Swapnil Metkar <swapnilmetkar83@gmail.com>
2023-03-28 19:38:20 +0530

    inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDocker1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, this is my docker learning.");
+        }
+    }
+}
